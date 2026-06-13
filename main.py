9d4d0b51950181b1efbe4a9d30a0e3aa81a322da
tunahidirli <thidirli@etu.edu.tr>
2026-06-13 19:51:51 +0300

    main py oluşturuldu

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("tuna")
+def toplama(a,b):
+    return a+b
+
+a=5
+if(a==10):
+
+    print("evet")
+
+else:
+    print("hayır")
+
+
+sayi1 = int(input("sayi giriniz"))
+sayi2 = int(input("sayi giriniz"))
+
+print(toplama(sayi1,sayi2))
